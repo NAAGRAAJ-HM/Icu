02951320b261bbf79ae978bd4ef9a0d7565a1b09
root <nagaraja.hm.0001@gmail.com>
2022-01-17 11:34:49 +0100

    Update: submodule Icu

--- LATEST/Icu.cpp
+++ LATEST/Icu.cpp
@@ -5,3 +5,60 @@ class_Icu Icu;
 FUNC(void, ICU_CODE) class_Icu::InitFunction(void){
 }
 
+FUNC(void, ICU_CODE) class_Icu::DeInitFunction(void){
+}
+
+FUNC(void, ICU_CODE) class_Icu::SetMode(void){
+}
+
+FUNC(void, ICU_CODE) class_Icu::DisableWakeup(void){
+}
+
+FUNC(void, ICU_CODE) class_Icu::SetActivationCondition(void){
+}
+
+FUNC(void, ICU_CODE) class_Icu::DisableNotification(void){
+}
+
+FUNC(void, ICU_CODE) class_Icu::EnableNotification(void){
+}
+
+FUNC(void, ICU_CODE) class_Icu::GetInputState(void){
+}
+
+FUNC(void, ICU_CODE) class_Icu::StartTimeStamp(void){
+}
+
+FUNC(void, ICU_CODE) class_Icu::StopTimeStamp(void){
+}
+
+FUNC(void, ICU_CODE) class_Icu::GetTimeStampIndex(void){
+}
+
+FUNC(void, ICU_CODE) class_Icu::ResetEdgeCount(void){
+}
+
+FUNC(void, ICU_CODE) class_Icu::DisableEdgeCount(void){
+}
+
+FUNC(void, ICU_CODE) class_Icu::EnableEdgeCount(void){
+}
+
+FUNC(void, ICU_CODE) class_Icu::GetEdgeNumbers(void){
+}
+
+FUNC(void, ICU_CODE) class_Icu::StartSignalMeasurement(void){
+}
+
+FUNC(void, ICU_CODE) class_Icu::StopSignalMeasurement(void){
+}
+
+FUNC(void, ICU_CODE) class_Icu::GetTimeElapsed(void){
+}
+
+FUNC(void, ICU_CODE) class_Icu::GetDutyCycleValues(void){
+}
+
+FUNC(void, ICU_CODE) class_Icu::GetVersionInfo(void){
+}
+
