81ac1ce684dc967aed67bb16716e8554828640ce
root <nagaraja.hm.0001@gmail.com>
2022-03-09 12:54:10 +0100

    Update: submodule Icu

--- LATEST/Icu_EcuM.h
+++ LATEST/Icu_EcuM.h
@@ -32,7 +32,7 @@
 /*****************************************************/
 /* OBJECTS                                           */
 /*****************************************************/
-extern infEcuMClient *gptrinfEcuMClient_Icu;
+extern infEcuMClient* gptrinfEcuMClient_Icu;
 
 /*****************************************************/
 /* FUNCTIONS                                         */
--- LATEST/Icu_SchM.h
+++ LATEST/Icu_SchM.h
@@ -32,7 +32,7 @@
 /*****************************************************/
 /* OBJECTS                                           */
 /*****************************************************/
-extern infSchMClient *gptrinfSchMClient_Icu;
+extern infSchMClient* gptrinfSchMClient_Icu;
 
 /*****************************************************/
 /* FUNCTIONS                                         */
