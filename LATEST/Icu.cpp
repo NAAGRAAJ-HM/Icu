25231164277feeb0f115ca6f5d33b58d6b76e0a3
root <nagaraja.hm.0001@gmail.com>
2022-03-23 13:07:49 +0100

    Update: AUTOSAR basic software micro-controller abstraction module Icu

--- LATEST/Icu.cpp
+++ LATEST/Icu.cpp
@@ -40,6 +40,16 @@ class module_Icu:
       FUNC(void, ICU_CODE) DeInitFunction (void);
       FUNC(void, ICU_CODE) GetVersionInfo (void);
       FUNC(void, ICU_CODE) MainFunction   (void);
+
+   private:
+      CONST(Std_TypeVersionInfo, ICU_CONST) VersionInfo = {
+            0x0000
+         ,  0xFFFF
+         ,  0x01
+         ,  '0'
+         ,  '1'
+         ,  '0'
+      };
 };
 
 /******************************************************************************/
