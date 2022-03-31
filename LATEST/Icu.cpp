bfd78623df60126e4f64c624140a72a2e8fbce1d
root <nagaraja.hm.0001@gmail.com>
2022-03-31 12:04:05 +0200

    Update: AUTOSAR basic software micro-controller abstraction module Icu

--- LATEST/Icu.cpp
+++ LATEST/Icu.cpp
@@ -37,10 +37,9 @@ class module_Icu:
    public:
       module_Icu(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
       }
-      FUNC(void, _CODE) InitFunction(
-         CONSTP2CONST(CfgModule_TypeAbstract, _CONFIG_DATA, _APPL_CONST) lptrCfgModule
+      FUNC(void, ICU_CODE) InitFunction(
+         CONSTP2CONST(CfgModule_TypeAbstract, ICU_CONFIG_DATA, ICU_APPL_CONST) lptrCfgModule
       );
-      FUNC(void, ICU_CODE) InitFunction   (void);
       FUNC(void, ICU_CODE) DeInitFunction (void);
       FUNC(void, ICU_CODE) MainFunction   (void);
 };
@@ -77,23 +76,39 @@ VAR(module_Icu, ICU_VAR) Icu(
 /* FUNCTIONS                                                                  */
 /******************************************************************************/
 FUNC(void, ICU_CODE) module_Icu::InitFunction(
-   CONSTP2CONST(CfgIcu_Type, CFGICU_CONFIG_DATA, CFGICU_APPL_CONST) lptrCfgIcu
+   CONSTP2CONST(CfgModule_TypeAbstract, ICU_CONFIG_DATA, ICU_APPL_CONST) lptrCfgModule
 ){
-   if(NULL_PTR == lptrCfgIcu){
+   if(E_OK == IsInitDone){
 #if(STD_ON == Icu_DevErrorDetect)
       Det_ReportError(
       );
 #endif
    }
    else{
-// check lptrCfgIcu for memory faults
+      if(NULL_PTR == lptrCfgModule){
+#if(STD_ON == Icu_DevErrorDetect)
+         Det_ReportError(
+         );
+#endif
+      }
+      else{
+// check lptrCfgModule for memory faults
 // use PBcfg_Icu as back-up configuration
+      }
+      IsInitDone = E_OK;
    }
-   Icu.IsInitDone = E_OK;
 }
 
 FUNC(void, ICU_CODE) module_Icu::DeInitFunction(void){
-   Icu.IsInitDone = E_NOT_OK;
+   if(E_OK != IsInitDone){
+#if(STD_ON == Icu_DevErrorDetect)
+      Det_ReportError(
+      );
+#endif
+   }
+   else{
+      IsInitDone = E_NOT_OK;
+   }
 }
 
 FUNC(void, ICU_CODE) module_Icu::MainFunction(void){
