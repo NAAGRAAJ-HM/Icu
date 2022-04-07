c1ac31b73382350af8c0502d211086df11298035
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-04-07 09:24:53 +0000

    Update: AUTOSAR basic software micro-controller abstraction driver module Icu

--- LATEST/Icu.cpp
+++ LATEST/Icu.cpp
@@ -101,8 +101,8 @@ FUNC(void, ICU_CODE) module_Icu::InitFunction(
             lptrCfg = lptrCfgModule;
          }
          else{
-// use PBcfg_CanIf as back-up configuration
-            lptrCfg = PBcfg_CanIf;
+// use PBcfgCanIf as back-up configuration
+            lptrCfg = &PBcfgIcu;
          }
       }
       IsInitDone = E_OK;
