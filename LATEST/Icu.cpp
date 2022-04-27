922df726e694bd07a2e47694c9af549ce004aa4d
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-04-27 10:44:23 +0000

    Update: AUTOSAR basic software micro-controller abstraction driver module Icu

--- LATEST/Icu.cpp
+++ LATEST/Icu.cpp
@@ -31,27 +31,6 @@
 /******************************************************************************/
 /* TYPEDEFS                                                                   */
 /******************************************************************************/
-class class_Icu_Functionality{
-   public:
-      FUNC(void, ICU_CODE) SetMode                (void);
-      FUNC(void, ICU_CODE) DisableWakeup          (void);
-      FUNC(void, ICU_CODE) SetActivationCondition (void);
-      FUNC(void, ICU_CODE) DisableNotification    (void);
-      FUNC(void, ICU_CODE) EnableNotification     (void);
-      FUNC(void, ICU_CODE) GetInputState          (void);
-      FUNC(void, ICU_CODE) StartTimeStamp         (void);
-      FUNC(void, ICU_CODE) StopTimeStamp          (void);
-      FUNC(void, ICU_CODE) GetTimeStampIndex      (void);
-      FUNC(void, ICU_CODE) ResetEdgeCount         (void);
-      FUNC(void, ICU_CODE) DisableEdgeCount       (void);
-      FUNC(void, ICU_CODE) EnableEdgeCount        (void);
-      FUNC(void, ICU_CODE) GetEdgeNumbers         (void);
-      FUNC(void, ICU_CODE) StartSignalMeasurement (void);
-      FUNC(void, ICU_CODE) StopSignalMeasurement  (void);
-      FUNC(void, ICU_CODE) GetTimeElapsed         (void);
-      FUNC(void, ICU_CODE) GetDutyCycleValues     (void);
-};
-
 class module_Icu:
       public abstract_module
    ,  public class_Icu_Functionality
